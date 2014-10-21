5742a2e3df9a7c4c19dc7590d8101c2104b7514a
mhyailz <250264787@qq.com>
2014-10-21 11:27:57 +0800

    加入文章评论

--- src/main/java/com/action/Index.java
+++ src/main/java/com/action/Index.java
@@ -1,5 +1,5 @@
-package com.action;    
-  
+package com.action;
+
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -14,61 +14,70 @@ import org.springframework.web.servlet.ModelAndView;
 import com.model.Article;
 import com.service.ArticleService;
 import com.utils.AESEncryptor;
-@Controller  
+@Controller
 @RequestMapping("/")
 public class Index {
-	
-	/** 密钥  **/
-	private static final String SECRETKEY = "abcdefghijklmnop" ;
-	
-	/** 服务 **/
-	@Autowired
-	private ArticleService articleService;
-	
+
+    /** 密钥  **/
+    private static final String SECRETKEY = "abcdefghijklmnop" ;
+
+    /** 服务 **/
+    @Autowired
+    private ArticleService articleService;
+
     @RequestMapping(value="index", method = RequestMethod.GET)
-	public ModelAndView indexPage(
-			@RequestParam(value = "ids", required = false) String ids) {
-		ModelAndView mv = new ModelAndView("article/index");
-		List<Article> articles = null;
-		if(StringUtils.isNotEmpty(ids)){
-			Integer id = null;
-			try{
-				id = new Integer(AESEncryptor.dec(ids, SECRETKEY));
-			}catch(Exception e){
-				System.out.print("解密报错:" + e);
-			}
-			if(id == null){
-				 articles = articleService.getAllArticleList(1, 10);
-			}else{
-				 articles = articleService.getArticleById(id);
-			}
-		}else{
-			 articles = articleService.getAllArticleList(1, 10);
-		}
-		if(!CollectionUtils.isEmpty(articles)){
-			for(Article article : articles){
-				article.setIds(AESEncryptor.enc(article.getId().toString(), SECRETKEY));
-			}
-		}
-		
-		mv.addObject("artilces", articles);
-		return mv;
-	}
+    public ModelAndView initPage() {
+        ModelAndView mv = new ModelAndView("article/index");
+        List<Article> articles = articleService.getAllArticleList(1, 10);
+        if(!CollectionUtils.isEmpty(articles)){
+            for(Article article : articles){
+                article.setIds(AESEncryptor.enc(article.getId().toString(), SECRETKEY));
+            }
+        }
+        mv.addObject("artilces", articles);
+        return mv;
+    }
+    @RequestMapping(value="article", method = RequestMethod.GET)
+    public ModelAndView getArticleById(@RequestParam(value = "ids", required = false) String ids){
+        ModelAndView mv = new ModelAndView("article/index");
+        List<Article> articles = null;
+        if(StringUtils.isNotEmpty(ids)){
+            Integer id = null;
+            try{
+                id = new Integer(AESEncryptor.dec(ids, SECRETKEY));
+            }catch(Exception e){
+                System.out.print("解密报错:" + e);
+            }
+            if(id == null){
+                articles = articleService.getAllArticleList(1, 10);
+            }else{
+                articles = articleService.getArticleById(id);
+            }
+        }
+        if(!CollectionUtils.isEmpty(articles)){
+            for(Article article : articles){
+                article.setIds(AESEncryptor.enc(article.getId().toString(), SECRETKEY));
+            }
+        }
+        mv.addObject("artilces", articles);
+        mv.addObject("isShowComme",1);
+        return mv;
+    }
 
-    
+
     @RequestMapping(value="m",method = RequestMethod.GET)
-	public String mIndexPage(){
-		return "article/mIndex";
-	}
-    
-    /**************************get set ************************/
-    
-	public ArticleService getArticleService() {
-		return articleService;
-	}
+    public String mIndexPage(){
+        return "article/mIndex";
+    }
 
-	public void setArticleService(ArticleService articleService) {
-		this.articleService = articleService;
-	}
-	
+    /**************************get set ************************/
+
+    public ArticleService getArticleService() {
+        return articleService;
+    }
+
+    public void setArticleService(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
 }  
